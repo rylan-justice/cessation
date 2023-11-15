072f5bf69fae37e013f998c0c780df2116b3e47a
rylan-justice <119366377+rylan-justice@users.noreply.github.com>
2023-11-14 23:26:45 -0500

    Rename variable for increased specificity

--- Cessation/Cessation.cmd
+++ Cessation/Cessation.cmd
@@ -24,9 +24,9 @@ set startup_script="%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Cess
 :main
   cls
   type data\menus\main.txt
-  set /p "menu_prompt=> " || goto main
+  set /p "command_prompt=> " || goto main
   cls
-  call :%menu_prompt%
+  call :%command_prompt%
   goto main
 
 :interlude
