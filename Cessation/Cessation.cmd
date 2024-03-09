1f8d649771952775d8279266861fc988344c52a9
rylan-justice <119366377+rylan-justice@users.noreply.github.com>
2024-03-09 08:29:40 -0500

    Use proper syntax

--- Cessation/Cessation.cmd
+++ Cessation/Cessation.cmd
@@ -1,20 +1,20 @@
-:: SPDX-License-Identifier: GPL-3.0-or-later
-::
-:: Cessation: Initiates the termination of startup processes.
-:: Copyright (C) 2023  Rylan Justice
-::
-:: This program is free software: you can redistribute it and/or modify
-:: it under the terms of the GNU General Public License as published by
-:: the Free Software Foundation, either version 3 of the License, or
-:: (at your option) any later version.
-::
-:: This program is distributed in the hope that it will be useful,
-:: but WITHOUT ANY WARRANTY; without even the implied warranty of
-:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-:: GNU General Public License for more details.
-::
-:: You should have received a copy of the GNU General Public License
-:: along with this program.  If not, see <https://www.gnu.org/licenses/>.
+rem SPDX-License-Identifier: GPL-3.0-or-later
+rem
+rem Cessation: Initiates the termination of startup processes.
+rem Copyright (C) 2023  Rylan Justice
+rem
+rem This program is free software: you can redistribute it and/or modify
+rem it under the terms of the GNU General Public License as published by
+rem the Free Software Foundation, either version 3 of the License, or
+rem (at your option) any later version.
+rem
+rem This program is distributed in the hope that it will be useful,
+rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem GNU General Public License for more details.
+rem
+rem You should have received a copy of the GNU General Public License
+rem along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 @echo off
 title %~n0
